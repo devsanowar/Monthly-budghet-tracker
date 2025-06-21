98205ae22ae4974afe0b33ab98adc4804ba0a478
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-21 20:12:54 +0600

    some column added in users table and migrate

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('system', ['super_admin','admin', 'staff', 'user'])->default('user');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
